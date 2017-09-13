3703fdc4f9a42b9cc2556486e9dd1023925ea946
brownae <aebrown9@gmail.com>
2017-09-12 21:50:13 -0700

    Adds credit for brownie wd in footer

--- client/includes/footer.php
+++ client/includes/footer.php
@@ -67,6 +67,7 @@
         </div>
     </div>
 
+<p><small><a href="http://browniewd.com/">Site baked by - Brownie Web Design </a></small></p>
 
 <!-- Where page divider came from. http://all-free-download.com/free-vector/download/vector-set-of-ornamental-design-rule-lines_569779.html http://all-free-download.com/free-vector/download/floral_design_elements_vintage_dividers_vector_set_570115_download.html
 http://all-free-download.com/free-vector/download/vintage_labels_and_borders_vector_520817_download.html
